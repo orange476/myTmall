870eada1603e59f33b56a7e698e39d7e0eb8b285
srybabex <39078205+srybabex@users.noreply.github.com>
2020-03-09 16:53:34 +0800

    Add files via upload

---
+++ mytmall/js/daohanghelunbo.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $("div.rightMenu span").mouseenter(function(){
+        var left = $(this).position().left;
+        var top = $(this).position().top;
+        var width = $(this).css("width");
+        var destLeft = parseInt(left) + parseInt(width)/2;
+        $("img#catear").css("left",destLeft);
+        $("img#catear").css("top",top-20);
+        $("img#catear").fadeIn(500);
+    });
+    $("div.rightMenu span").mouseleave(function(){
+        $("img#catear").hide();
+    });
+});
